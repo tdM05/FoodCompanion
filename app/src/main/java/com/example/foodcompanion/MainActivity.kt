f8cacbff57a429f6ae8da94417ff599f3de9990f
tdM05 <taddmao@gmail.com>
2024-07-08 13:51:05 -0700

    Added some files to organize things. Notably each screen (each different page) will have a different file and then the screen manager will switch between the screens). Also, I added a date of birth field in the login screen.

--- app/src/main/java/com/example/foodcompanion/MainActivity.kt
+++ app/src/main/java/com/example/foodcompanion/MainActivity.kt
@@ -26,6 +26,8 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import uicommunicator.*
+import com.example.foodcompanion.screens.LoginPage
+
 
 
 class MainActivity : ComponentActivity() {
@@ -48,52 +50,5 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun LoginPage(modifier: Modifier = Modifier) {
-    val spaceHeight = 26.dp
-    Column (
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Top,
-        horizontalAlignment = Alignment.CenterHorizontally) {
-        Spacer(modifier = Modifier.height(100.dp))
-        // This is the title
-        Text(text = "Food Companion", fontSize = 60.sp, lineHeight = 60.sp, textAlign = TextAlign.Center)
 
-        Spacer(modifier = Modifier.height(spaceHeight))
-
-        //patient ID
-        var patientID by remember { mutableStateOf("") }
-        TextField(
-            value = patientID,
-            onValueChange = { patientID = it },
-            label = { Text("patient ID") },
-            modifier = modifier,
-        )
-
-        Spacer(modifier = Modifier.height(spaceHeight))
-
-        //institutionID
-        var institutionID by remember { mutableStateOf("") }
-        TextField(
-            value = institutionID,
-            onValueChange = { institutionID = it },
-            label = { Text("Institution ID") },
-            modifier = modifier,
-        )
-
-        Spacer(modifier = Modifier.height(spaceHeight))
-
-        //This is the button
-        Button(onClick = { verifyID(patientID, institutionID) }) {
-            Text("Log In")
-        }
-    }
-}
-
-
-@Preview(showBackground = true)
-@Composable
-fun TextFieldPreview() {
-    LoginPage()
-}
 
--- app/src/main/java/uicommunicator/LoginCalls.kt
+++ app/src/main/java/uicommunicator/LoginCalls.kt
@@ -1,17 +1,25 @@
 package uicommunicator
 import android.util.Log
 
-fun verifyID(patientID: String, institutionID: String = "") {
+fun verifyID(patientID: String, institutionID: String, birthday: String) {
     /*
-    This function is called after the user enters a valid Institution and school id and presses enter
+    This function is called after the user enters a valid Institution and school id and presses enter.
+    This should communicate with the server to request validation of the user profile.
+    If the server validates their request, then this function will call the next main screen.
 
     Note: open LogCat and put a tag of "debug" to view below messages
      */
 
     Log.d(
         "debug", "patientID: " + patientID + ", " + "institutionID: " +
-                institutionID
+                institutionID + ", birthday: " + birthday
     )
+
+    var accepted = true  // the server should verify this!
+
+    if (accepted) {
+
+    }
 }
 
 
