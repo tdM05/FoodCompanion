fa3650df899fbab41f811926c39936500db24904
elhadji <elhadjisall2005@gmail.xom>
2024-07-09 20:20:39 -0700

    chat i added dataclass for food, category, fooddata added gson serialization and deserialization in fooddatamanager

---
+++ app/src/main/java/com/example/foodcompanion/Data/FoodDataManager.kt
@@ -0,0 +1,17 @@
+package com.example.foodcompanion.data
+
+import com.google.gson.Gson
+
+class FoodDataManager {
+    private val gson = Gson()
+
+    fun fromJson(json: String): FoodData.FoodData {
+        return gson.fromJson(json, FoodData.FoodData::class.java)
+    }
+
+    fun toJson(foodData: FoodData.FoodData): String {
+        return gson.toJson(foodData)
+    }
+}
+
+
