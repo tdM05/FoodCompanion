2b20f935511b15f904158860adee56dffe3c4485
Geetansh Gautam <33170989+GeetanshGautam0@users.noreply.github.com>
2024-08-13 12:18:11 -0700

    Revert "Dev"

--- ncom/sc_data/__init__.py
+++
@@ -1,7 +0,0 @@
-from . import constants as Constants
-from . import functions as Functions
-from . import settings as Settings
-from . import enum as Enums
-from . import struct as Structs
-from . import header as Header
-from . import appinfo as AppInfo
